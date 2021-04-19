bf0d47aac51984e7999c396428d3dc2c74517797
pogowurst <pogowurst87@googlemail.com>
2021-04-19 21:42:34 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os.path
+
+from setuptools import setup
+
+readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+with open(readme) as f:
+    long_description = f.read()
+
+setup(
+    name='django-galera',
+    version='0.1',
+    description='Django database backend for MariaDB Galera Cluster',
+    long_description=long_description,
+    url='https://github.com/pogowurst/django-galera',
+    author='Steve Hunger',
+    author_email='pogowurst87@googlemail.com',
+    license='MIT',
+    packages=(
+        'galera',
+    ),
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+    ),
+    install_requires=(
+        'django>=2.2'
+    )
+)
